ed11d137b9a6cbccd8f22cebda50f3174c5a639d
Geo <tp53.geo@gmail.com>
2024-08-12 09:41:47 +0200

    change method to a template for both remote type

--- src/Inputs/CardputerInput.cpp
+++ src/Inputs/CardputerInput.cpp
@@ -43,53 +43,5 @@ char CardputerInput::handler() {
     return KEY_NONE;
 }
 
-std::vector<char> CardputerInput::getCommandMappings(const std::vector<RemoteCommand>& commands) {
-    std::vector<char> mappings(commands.size(), '>'); // mapped keys for each command
-    std::vector<char> usedKeys; // already assigned keys
-    std::vector<std::pair<size_t, char>> defaultKeyBindings; // (index, default key) pairs
-
-    // Collect default keys to avoid conflict
-    for (size_t i = 0; i < commands.size(); ++i) {
-        std::string cleanName = utils::StringUtils::cleanCommandName(commands[i].functionName);
-        auto it = keyMappings.find(cleanName);
-        if (it != keyMappings.end()) {
-            char mappedKey = it->second;
-            if (std::find(usedKeys.begin(), usedKeys.end(), mappedKey) == usedKeys.end()) {
-                mappings[i] = mappedKey;
-                usedKeys.push_back(mappedKey);
-            } else {
-                defaultKeyBindings.push_back({i, mappedKey});
-            }
-        }
-    }
-
-    // Assign others keys
-    for (size_t i = 0; i < commands.size(); ++i) {
-        if (mappings[i] == '>') {
-            bool keyFound = false;
-            for (char key : assignableKeys) {
-                if (std::find(usedKeys.begin(), usedKeys.end(), key) == usedKeys.end()) {
-                    mappings[i] = key;
-                    usedKeys.push_back(key);
-                    keyFound = true;
-                    break;
-                }
-            }
-            // stay at default '>' (no keybind)
-        }
-    }
-
-    // Assign default keys
-    for (const auto& binding : defaultKeyBindings) {
-        size_t index = binding.first;
-        char defaultKey = binding.second;
-        if (std::find(usedKeys.begin(), usedKeys.end(), defaultKey) == usedKeys.end()) {
-            mappings[index] = defaultKey;
-            usedKeys.push_back(defaultKey);
-        }
-    }
-
-    return mappings;
-}
 
 }
